c97b8351a09bd254a27762ba0940e1794146a242
Lukas Snoek <lukassnoek@gmail.com>
2019-02-07 15:37:05 +0100

    Add log

--- nitools/cmd_tools.py
+++ nitools/cmd_tools.py
@@ -7,7 +7,10 @@ import os.path as op
 import bidsify
 import subprocess
 import click
+import logging
+import sys
 from glob import glob
+from datetime import datetime
 from bidsify import bidsify as run_bidsify
 from bidsify.docker import run_from_docker as run_bidsify_docker
 from .preproc import run_preproc
@@ -51,7 +54,27 @@ def run_qc_and_preproc(project=None, docker=False):
     cp_file = op.join(op.dirname(__file__), 'data', 'CURRENT_PROJECTS.yml')
     with open(cp_file, 'r') as cpf:
         curr_projects = yaml.load(cpf)
-    
+   
+    date = datetime.now().strftime("%Y-%m-%d")
+    log_dir = op.join(env['server_home'], 'logs')
+    log_file = op.join(log_dir, 'nitools_%s.txt' % date)
+    formatter = logging.Formatter('%(message)s')
+
+    logger = logging.getLogger("nitools-logger")
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)    
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.DEBUG)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    logger.info("Starting nitools at %s" % str(datetime.now()))
+
     # Loop over projects
     for proj_name, settings in curr_projects.items():
 
@@ -62,7 +85,7 @@ def run_qc_and_preproc(project=None, docker=False):
             if not settings['run_automatically']:
                 continue
     
-        print("\n======== PROCESSING DATA FROM PROJECT %s ========" % proj_name)
+        logger.info("\n======== PROCESSING DATA FROM PROJECT %s ========" % proj_name)
 
         export_folder = settings['export_folder']
         if 'fMRI Project' in export_folder:
@@ -71,8 +94,9 @@ def run_qc_and_preproc(project=None, docker=False):
             export_folder = op.join(env['dropbox'], export_folder)
 
         if not op.isdir(export_folder):
-            raise ValueError("The export-folder '%s' doesn't seem to"
-                             " exist!" % export_folder)
+            msg = "The export-folder '%s' doesn't seem to exist!" % export_folder
+            logger.error(msg)
+            raise ValueError(msg)
 
         proj_dir = op.join(env['server_home'], proj_name)
         # Check for raw subjects
@@ -95,22 +119,20 @@ def run_qc_and_preproc(project=None, docker=False):
             if not op.isdir(server_dir):
                 n_files = len(os.listdir(sub))
                 if n_files > 0:
-                    print("Copying data from %s to server ..." % sub_idf, end=' ')
+                    logger.info("Copying data from %s to server ..." % sub_idf)
                     shutil.copytree(sub, server_dir)
-                    print("done.")
             else:
                 pass
-                # print("Data from %s is already on server!" % sub_idf)
 
         # Also check for config.yml
         cfg_file = op.join(export_folder, 'raw', 'config.yml')
         if op.isfile(cfg_file):
-            print("Copying config file to server ...")
+            logger.info("Copying config file to server ...")
             shutil.copy2(cfg_file, op.join(proj_dir, 'raw'))
             cfg_file = op.join(proj_dir, 'raw', 'config.yml')
 
         # Then bidsify everything
-        print("\n-------- RUNNING BIDSIFY --------")
+        logger.info("\n-------- RUNNING BIDSIFY --------")
         if op.isfile(cfg_file):
             this_cfg = cfg_file
         else:
@@ -140,7 +162,7 @@ def run_qc_and_preproc(project=None, docker=False):
         for sub in bids_files_on_server:
             sub_name = op.basename(sub)
             if not op.isdir(op.join(bids_export_folder, sub_name)):
-                print('Copying files from %s to export-dir ...' % sub)
+                logger.info('Copying files from %s to export-dir ...' % sub)
                 shutil.copytree(sub, op.join(bids_export_folder, sub_name))
         
         participants_file = op.join(bids_out_dir, 'participants.tsv')
@@ -149,12 +171,12 @@ def run_qc_and_preproc(project=None, docker=False):
         shutil.copyfile(dataset_descr_file, op.join(bids_export_folder, op.basename(dataset_descr_file)))
         
         if settings['preproc']:
-            print("\n-------- RUNNING FMRIPREP --------")
+            logger.info("\n-------- RUNNING FMRIPREP --------")
             run_preproc(bids_dir=op.join(proj_dir, 'bids'),
                         export_dir=export_folder,
                         **settings['fmriprep_options'])
 
         if settings['qc']:
-            print("\n-------- RUNNING MRIQC --------")
+            logger.info("\n-------- RUNNING MRIQC --------")
             run_qc(bids_dir=op.join(proj_dir, 'bids'),
                    export_dir=export_folder, **settings['mriqc_options'])
