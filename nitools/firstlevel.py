6cc240ad763f10aa986483b6112e9b4c1097eb01
Lukas Snoek <lukassnoek@gmail.com>
2019-11-28 16:57:43 +0100

    Allow for ffx analysis to return other than zstats

--- nitools/firstlevel.py
+++ nitools/firstlevel.py
@@ -45,8 +45,8 @@ class Dataset:
         self.fp_dir = op.join(self.deriv_dir, 'fmriprep', f'sub-{sub}{self.postfix}')
         self.fs_dir = op.join(self.deriv_dir, 'freesurfer', f'sub-{sub}')
 
-    def create_taskset(self, task, space, acq=None, ses=False, hemi='R', reference_run=-1, use_gm_mask=False,
-                       gm_threshold=0):
+    def create_taskset(self, task, space, tr, acq=None, ses=False, hemi='R', reference_run=-1, use_gm_mask=True,
+                       gm_threshold=0.5):
         """ Creates a 'taskset'. 
         
         parameters
@@ -55,6 +55,8 @@ class Dataset:
             Name of task
         space : str
             Name of space to analyze data in (e.g., 'T1w', 'fsnative')
+        tr : float
+            Time to repetition
         acq : str
             Name of acquisition parameter (in filenames, e.g., acq-Mm3)
         ses : bool
@@ -147,10 +149,10 @@ class Dataset:
         
         n_comp_runs = len(data['funcs'])
         print(f"Found {n_comp_runs} complete runs for task {task}")
-        ts = Taskset(task=task, space=space, n_jobs=self.n_jobs, **data)
+        ts = Taskset(tr=tr, task=task, space=space, n_jobs=self.n_jobs, **data)
         setattr(self, task, ts)
 
-    def visualize(self, statmap, space='fsnative', threshold=0, hemi='R', **plot_args):
+    def visualize(self, statmap, space='fsnative', threshold=0, hemi='R', mask=None, **plot_args):
         """ Visualizes a statmap on an image/surface background. """
         if 'fs' in space and isinstance(statmap, nib.Nifti1Image):
             raise ValueError("Statmap is an image, but space is fs* something ...")
@@ -172,6 +174,10 @@ class Dataset:
                 **plot_args
             )
         else:
+            
+            if mask is not None:
+                statmap = masking.unmask(statmap, mask)
+
             if space == 'T1w':
                 bg = op.join(self.fp_dir, 'anat', f'sub-{self.sub}_desc-preproc_T1w.nii.gz')
             else:
@@ -203,7 +209,7 @@ class Dataset:
             rfv = rfv.loc[rfv.index.get_level_values(0) == condition, :]
         
         if rsq_cutoff is not None:
-            rsq = rf.get_rsq()
+            rsq = np.squeeze(rf.get_rsq())
             dat = rfv.values[:, rsq > rsq_cutoff]
         else:
             dat = rfv.values[:, mask]
@@ -224,14 +230,17 @@ class Dataset:
         ax[1].set_ylabel("Frequency", fontsize=20)
         sns.distplot(t[dat.argmax(axis=0)], ax=ax[1])
         sns.despine()
-        
+
+
 class Taskset:
     
-    def __init__(self, task, space, funcs, confs, events, ricors, mask, gm_mask=None, n_jobs=1):
+    def __init__(self, tr, task, space, funcs, confs, events, ricors, mask, gm_mask=None, n_jobs=1):
         """ Initializes a taskset object. 
         
         Parameters
         ----------
+        tr : float
+            Time to repetition
         task : str
             Name of task
         space : str
@@ -249,6 +258,7 @@ class Taskset:
         n_jobs : int
             Number of jobs to use for computations
         """
+        self.tr = tr
         self.task = task
         self.space = space
         self.funcs = funcs
@@ -266,11 +276,12 @@ class Taskset:
         msg = f"{self.task} taskset containg data of {self.n_runs} runs"
         return msg
     
-    def preprocess(self, smoothing_fwhm=None, conf_vars=None, df_filter=None, regress_confounds=False):
+    def preprocess(self, smoothing_fwhm=None, hp_cutoff=None, conf_vars=None, df_filter=None,
+                   slice_time_ref=0.5, regress_confounds=False):
         print(f"Preprocessing data for task {self.task} ...")
         if conf_vars is None:
             conf_vars = ['trans_x', 'trans_y', 'trans_z', 'rot_x', 'rot_y', 'rot_z']
-        
+                
         out = Parallel(n_jobs=self.n_jobs)(delayed(_run_preproc_in_parallel)(
                 run=run,
                 event=self.events[run],
@@ -281,7 +292,10 @@ class Taskset:
                 conf_vars=conf_vars,
                 mask=self.mask,
                 smoothing_fwhm=smoothing_fwhm,
-                df_filter=df_filter
+                hp_cutoff=hp_cutoff,
+                df_filter=df_filter,
+                tr=self.tr,
+                slice_time_ref=slice_time_ref
             ) for run in range(self.n_runs)
         )
         
@@ -329,24 +343,31 @@ class Taskset:
             
             this_stat[np.isnan(this_stat)] = 0
             stat[run, :] = this_stat
-            
-        return stat.mean(axis=0)
+        
+        mean_stat = stat.mean(axis=0)
+        if self.mask is not None:
+            mean_stat = masking.unmask(mean_stat, self.mask)
+        
+        return mean_stat
     
-    def glm_detect(self, hrf_model='glover', noise_model='ols', osf=30, TR=1.317, mask=None, rf_condition=None):
+    def glm_detect(self, dm=None, hrf_model='glover', noise_model='ols', osf=30,
+                   slice_time_ref=0.5, mask=None, rf_condition=None):
 
         if not self.preprocessed:
             raise ValueError("Data was not preprocessed yet!")
 
         print(f"Running a GLM for task {self.task} ...")
         glm_out = Parallel(n_jobs=self.n_jobs)(delayed(_run_glm_in_parallel)(
+            dm=dm,
             run=run,
             event=self.preproc['events'][run],
             conf=self.preproc['confs'][run],
             func=self.preproc['func_ts'][run],
             hrf_model=hrf_model,
             noise_model=noise_model,
-            TR=TR,
+            tr=self.tr,
             osf=osf,
+            slice_time_ref=slice_time_ref,
             mask=mask,
             rf_condition=rf_condition
             ) for run in range(self.n_runs)
@@ -365,40 +386,38 @@ class Taskset:
         if len(glm_out[0]) == 4:
             self.glm['hrfs'] = [run[3] for run in glm_out]
 
-    def shape_estimation(self, smoothing_fwhm=None, conf_vars=None, osf=30, TR=1.317,
+    def shape_estimation(self, conf_vars=None, osf=30, TR=1.317, slice_time_ref=0.5,
                          separate_conditions=False, **rf_args):
         
         if not self.preprocessed:
             raise ValueError("Data was not preprocessed yet!")
 
         print(f"Estimating HRF shape for task {self.task} ...")
+        func_cleans, concat_events = [], []
         for run in range(self.n_runs):
-            event, conf, func = self.preproc['events'][run], self.preproc['confs'][run], self.preproc['func_ts'][run]
-            event.loc[:, 'onset'] += run * conf.shape[0] * TR
-            self.preproc['events'][run] = event
+            this_conf = self.preproc['confs'][run].values
+            func_clean = signal.clean(
+                signals=self.preproc['func_ts'][run],
+                confounds=this_conf,
+                detrend=False,
+                standardize=True,
+                t_r=self.tr
+            )
+            func_cleans.append(func_clean)
+            
+            event = self.preproc['events'][run].copy()  
+            # copy, otherwise shape_estimation interferes with glm_detect
+            event.loc[:, 'onset'] += run * this_conf.shape[0] * TR
+            concat_events.append(event)
 
-        func_concat = np.concatenate(self.preproc['func_ts'])
-        conf_concat = pd.concat(self.preproc['confs'], axis=0)
-        event_concat = pd.concat(self.preproc['events'], axis=0)
+        func_concat = np.concatenate(func_cleans)
+        event_concat = pd.concat(concat_events, axis=0)#self.preproc['events'], axis=0)
 
-        ses_id = np.concatenate(
-            [[i+1] * self.preproc['func_ts'][i].shape[0]
-            for i in range(self.n_runs)]
-        )
-
-        func_clean = signal.clean(
-            signals=func_concat,
-            sessions=ses_id,
-            confounds=conf_concat.values,
-            detrend=False,
-            standardize=True,
-            t_r=TR
-        )
-        
         rf = ResponseFitter(
-            input_signal=func_clean,
+            input_signal=func_concat,
             sample_rate=1/TR,
             add_intercept=True,
+            slice_time_ref=slice_time_ref,
             oversample_design_matrix=osf
         )
         
@@ -418,10 +437,10 @@ class Taskset:
         else:
             rf.add_event('stim', event_concat.onset, **rf_args)
 
-        rf.regress()
+        rf.fit()
         self.rf = rf
 
-    def compute_fxe(self, contrast_def, stat_type='t', run=None):
+    def compute_fxe(self, contrast_def, stat_type='t', run=None, output_type='z_score'):
         """ Computes a fixed effect across multiple runs. """
         print(f"Computing contrast: {contrast_def} for task {self.task} ...")
         if self.glm is None:
@@ -455,23 +474,36 @@ class Taskset:
         else:
             contrast = compute_contrast(labels, results, con_vals, stat_type)
 
-        z = contrast.z_score()
+        values = getattr(contrast, output_type)()
         if self.mask is not None:
-            return masking.unmask(z, self.mask)
+            return masking.unmask(values, self.mask)
         else:
-            return z
+            return values
+        
+    def plot_design(self, exclude_confs=True):
+        n_runs = len(self.glm['dms'])
+        fig, ax = plt.subplots(nrows=n_runs, figsize=(15, 4 * n_runs))
+        for run in range(n_runs):
+            
+            if n_runs != 1:
+                this_ax = ax[run]
+            else:
+                this_ax = ax
+            
+            this_dm = self.glm['dms'][run].drop('constant', axis=1)
+            if exclude_confs:
+                this_dm = this_dm.drop(self.glm['confs'][run].columns, axis=1)
+
+            this_ax.plot(this_dm.values)
+            this_ax.set_title(f"Run {run+1}")
+            if run == 0:
+                this_ax.legend(this_dm.columns)
         
 
-def _run_preproc_in_parallel(run, event, conf, func, task, space, conf_vars, mask, smoothing_fwhm, df_filter):
+def _run_preproc_in_parallel(run, event, conf, func, task, space, conf_vars, mask, hp_cutoff,
+                             smoothing_fwhm, df_filter, slice_time_ref, tr):
     print(f"\tPreparing run {run+1} ...")
-    event = pd.read_csv(event, sep='\t')
-    if df_filter is not None:
-        event = df_filter(event)
     
-    conf = pd.read_csv(conf, sep='\t')
-    all_conf_vars = conf_vars + [col for col in conf.columns if 'cosine' in col]
-    conf = conf.loc[:, all_conf_vars].fillna(0)
-
     if 'fs' in space:
         func_ts = np.vstack([d.data[np.newaxis, :] for
                              d in nib.load(func).darrays])
@@ -486,84 +518,123 @@ def _run_preproc_in_parallel(run, event, conf, func, task, space, conf_vars, mas
             smoothing_fwhm=smoothing_fwhm
         )
 
+    n_vols = func_ts.shape[0]
+    start_time = slice_time_ref * tr
+    end_time = (n_vols - 1 + slice_time_ref) * tr
+    frame_times = np.linspace(start_time, end_time, n_vols)
+
+    event = pd.read_csv(event, sep='\t')
+    if df_filter is not None:
+        event = df_filter(event)
+    
+    conf = pd.read_csv(conf, sep='\t')    
+    all_conf_vars = conf_vars
+
+    mo = [col for col in conf.columns if 'motion_outlier' in col]
+    if len(mo) > 0:
+        print(f"\tAdding {len(mo)} motion outliers to design for run {run+1}...")
+        all_conf_vars += mo
+
+    if hp_cutoff is None:
+        all_conf_vars += [col for col in conf.columns if 'cosine' in col]
+        conf = conf.loc[:, all_conf_vars].fillna(0)
+    else:
+        conf = conf.loc[:, all_conf_vars].fillna(0)
+        cos = _create_cosine_set(frame_times, period_cut=hp_cutoff)
+        cos_names = [f'cosine{ic}'.zfill(3) for ic in range(cos.shape[1])]
+        cos_df = pd.DataFrame(data=cos, columns=cos_names, index=conf.index)
+        conf = pd.concat((conf, cos_df), axis=1)
+            
     return func_ts, conf, event
 
 
-def _run_glm_in_parallel(run, event, conf, func, hrf_model, noise_model, TR, osf, mask, rf_condition):
-    print(f"\tFitting run {run+1} ...")
-    frame_times = np.arange(0, conf.shape[0] * TR, TR)
-    
+def _run_glm_in_parallel(dm, run, event, conf, func, hrf_model, noise_model, tr, osf, slice_time_ref, 
+                         mask, rf_condition):
+
+    print(f"\tFitting GLM to run {run+1} ...")
+    n_vols = func.shape[0]
+    start_time = slice_time_ref * tr
+    end_time = (n_vols - 1 + slice_time_ref) * tr
+    frame_times = np.linspace(start_time, end_time, n_vols)
+
     if mask is not None:
         func = func[:, mask.ravel()]
-    
-    if not isinstance(hrf_model, str):  # custom HRF!
-        n_vols = func.shape[0]
-        conds = event.trial_type.unique()
-        hr_frame_times = np.arange(0, TR * n_vols, TR / osf)[:-osf+1]
-        cols = ['constant'] + conds.tolist()
-        
+
+    if dm is not None:
+        dm.index = frame_times
+        # skip to below
+    elif not isinstance(hrf_model, str):  # custom HRF!
+        conds = sorted(event.trial_type.unique())
+        cols = ['constant'] + conds
+
         if isinstance(hrf_model, np.ndarray):
+            # It's a SINGLE HRF (not a voxel-specific)
             X = np.zeros((n_vols, len(conds) + 1))
-            X[:, 0] = 1
-            for i, con in enumerate(conds):
+            X[:, 0] = 1  # intercept
+
+            for i, con in enumerate(conds):  # loop over conditions
                 trials = event.query('trial_type == @con')
-                x = np.zeros(hr_frame_times.size)
-                for ii in range(trials.shape[0]):
-                    row = trials.iloc[ii, :]
-                    start = np.argmin(np.abs(hr_frame_times - row.onset))
-                    end = start + int(row.duration / (TR / osf))
-                    x[start:end] = 1
+                exp_cond = trials.loc[:, ['onset', 'duration', 'weight']]
+                exp_cond['weight'] = 1
+
+                # Upsample
+                x, hr_frame_times = _sample_condition(exp_cond.values.T, frame_times, oversampling=osf)
+
+                # Convolve predictor
                 xconv = np.convolve(x, hrf_model)[:x.shape[0]]
+
+                # Downsample
                 f_interp = interp1d(hr_frame_times, xconv)
                 X[:, i+1] = f_interp(frame_times)
-            dm = pd.DataFrame(data=X, columns=cols, index=frame_times)
-        elif isinstance(hrf_model, ResponseFitter):
-            # Assuming it's a per-voxel GLM
-            
-            corrs = np.sqrt(hrf_model.get_rsq())
-            corr_max = corrs.max()
-            hrf_ts = hrf_model.get_timecourses()
-            if rf_condition is not None:
-                hrf_ts = hrf_ts.loc[hrf_ts.index.get_level_values(0) == rf_condition, :]
 
-            tlength = hrf_ts.index.get_level_values(-1).values[-1]
-            hrf_values = hrf_ts.values[:-1, :]
-            
+            # Save the design matrix
+            dm = pd.DataFrame(data=X, columns=cols, index=frame_times)
+
+        elif isinstance(hrf_model, ResponseFitter):
+
+            # Assuming it's a per-voxel GLM
+            hrf_tc = hrf_model.get_timecourses()
+            if rf_condition is not None:
+                hrf_tc = hrf_tc.loc[hrf_tc.index.get_level_values(0) == rf_condition, :]
+
+            tlength = hrf_tc.index.get_level_values(-1).values[-1]
+            hrf_values = hrf_tc.values
+
             n_vox = func.shape[1]
             labels, results = [], {}
             canon = glover_hrf(tr=TR, oversampling=osf, time_length=tlength, onset=0.0)
-            canon /= canon.max()
+            #canon /= canon.max()
+
             hrfs = np.zeros((3, canon.size, n_vox))
-            for vox in tqdm_notebook(range(n_vox), desc=f'run {run+1}'):
+            for vox in range(n_vox):
+                
+                if vox % 1000 == 0:
+                    print(f"Voxel {vox} / {n_vox} (run {run + 1})")
+                
                 this_hrf = hrf_values[:, vox]
-                this_hrf /= this_hrf.max()
+                #this_hrf /= this_hrf.max()
                 hrfs[0, :, vox] = this_hrf
                 hrfs[1, :, vox] = canon
+                #hrfs[2, :, vox] = this_hrf
 
-                this_corr = corrs[vox]
-                if this_corr < 0:
-                    this_corr = 0
-                
-                this_hrf = (this_hrf * (this_corr / corr_max) + canon * ((1 - corr_max) / corr_max)) / 2 
-                hrfs[2, :, vox] = this_hrf
-                hrfs[:, vox]
                 X = np.zeros((n_vols, len(conds) + 1))
                 X[:, 0] = 1  # icept
                 for i, con in enumerate(conds):
                     trials = event.query('trial_type == @con')
-                    x = np.zeros(hr_frame_times.size)
-                    for ii in range(trials.shape[0]):
-                        row = trials.iloc[ii, :]
-                        start = np.argmin(np.abs(hr_frame_times - row.onset))
-                        end = start + int(row.duration / (TR / osf))
-                        x[start:end] = 1
+                    exp_cond = trials.loc[:, ['onset', 'duration', 'weight']]
+                    exp_cond['weight'] = 1
+                    x, hr_frame_times = _sample_condition(exp_cond.values.T, frame_times, oversampling=osf)
+
                     xconv = np.convolve(x, this_hrf)[:x.shape[0]]
+                    #xconv = np.convolve(x, canon)[:x.shape[0]]
                     f_interp = interp1d(hr_frame_times, xconv)
                     X[:, i+1] = f_interp(frame_times)
-                
-                lab, res = run_glm(func[:, vox, np.newaxis], X, noise_model=noise_model)
+
+                X = pd.DataFrame(data=X, columns=cols, index=frame_times)
+                conf.index = X.index
+                dm = pd.concat((X, conf), axis=1)
+                lab, res = run_glm(func[:, vox, np.newaxis], dm.values, noise_model=noise_model)
                 labels, results = _merge_regression_results(lab[0], res, labels, results, n_vox=n_vox)
-            dm = pd.DataFrame(data=X, columns=cols, index=frame_times)
             return np.array(labels), results, dm, hrfs
         else:
             raise ValueError("Unknown type for hrf_model; don't know what to do with it!")
@@ -575,6 +646,7 @@ def _run_glm_in_parallel(run, event, conf, func, hrf_model, noise_model, TR, osf
             hrf_model=hrf_model,
             fir_delays=np.arange(1, 10)
         )
+
     conf.index = dm.index
     dm = pd.concat((dm, conf), axis=1)
     glm_results = run_glm(func, dm.values, noise_model=noise_model)
@@ -595,8 +667,8 @@ def _create_cosine_set(frame_times, period_cut=0.01):
         cosine_drift[:, k - 1] = normalizer * np.cos(
             (np.pi / n_frames) * (n_times + .5) * k)
 
-    cosine_drift[:, -1] = 1.
-    return cosine_drift
+    #cosine_drift[:, -1] = 1.
+    return cosine_drift[:, :-1]
 
 
 def _merge_regression_results(lab, res, labels, results, n_vox):
